87235b381f715f6a0bae0d059e4703eac0a22fe3
cbrzn <cesarbrazon10@gmail.com>
2018-04-20 02:56:33 -0400

    first

---
+++ client/reducers/comments.js
@@ -0,0 +1,10 @@
+// a reducer takes in two this:
+// 1. the action (information about what happend)
+// 2. copy of the current state
+
+
+function comments(state = [], action) {
+  return state;
+}
+
+export default comments;
---
+++ client/reducers/posts.js
@@ -0,0 +1,20 @@
+// a reducer takes in two this:
+// 1. the action (information about what happend)
+// 2. copy of the current state
+
+
+function posts(state = [], action) {
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+    const i = action.index;
+    return [
+      ...state.slice(0,i),
+      {...state[i], likes: state[i].likes + 1},
+      ...state.slice(i + 1),
+    ]
+    default:
+    return state;
+  }
+}
+
+export default posts;
